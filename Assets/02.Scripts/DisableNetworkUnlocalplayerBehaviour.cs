daedd0adeef5b6b60b1d4dd9bc454611ad24c99b
Monma8 <freaksfiler@gmail.com>
2015-07-10 12:04:35 +0900

    7/10授業終了

---
+++ Assets/02.Scripts/DisableNetworkUnlocalplayerBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class DisableNetworkUnlocalplayerBehaviour : NetworkBehaviour {
+	//[SerializeField]
+	//Behaviour[] behaviours;
+	public Behaviour cntrlScript;
+	//public AudioSource ausioS;
+	public Camera camera;
+	// Use this for initialization
+	void Start () {
+		if(!isLocalPlayer)
+		{
+			cntrlScript.enabled = false;
+			//ausioS.enabled = false;
+			camera.enabled = false;
+		}
+	}
+	
+	void OnApplicationFocus(bool focusStatus)
+	{
+		if(isLocalPlayer)
+		{
+			cntrlScript.enabled = focusStatus;
+			//ausioS.enabled = focusStatus;
+			camera.enabled = true;
+		}
+	}
+	
+}
