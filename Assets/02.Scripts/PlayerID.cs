2b31eaffcd312ff78fb13afe32d8b186c063378f
Monma8 <freaksfiler@gmail.com>
2015-07-17 12:12:29 +0900

    7/17終了

---
+++ Assets/02.Scripts/PlayerID.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerID : NetworkBehaviour {
+	[SyncVar]
+	private string playerUniqueIdentity;
+	
+	private NetworkInstanceId playerNetID;
+	private Transform myTransform;
+	
+	public override void OnStartLocalPlayer()
+	{
+		Debug.Log ("OnStartLocalPlayer");
+		GetNetIdentity();
+		SetIdentity();
+	}
+	
+	void Awake()
+	{
+		Debug.Log("Awake");
+		myTransform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(myTransform.name == ""||myTransform.name == "Player(Clone)")
+		{
+			SetIdentity();
+		}
+	}
+	
+	[Client]
+	void GetNetIdentity()
+	{
+		playerNetID = GetComponent<NetworkIdentity>().netId;
+		CmdTellServerMyIdentity(MakeUniqueIdentity());
+	}
+	
+	void SetIdentity()
+	{
+		if(!isLocalPlayer)
+		{
+			myTransform.name = playerUniqueIdentity;
+		}else{
+			myTransform.name = MakeUniqueIdentity();
+		}
+	}
+	
+	string MakeUniqueIdentity()
+	{
+		string uniqueName = "Player" + playerNetID.ToString();
+		return uniqueName; 
+	}
+	
+	//Command: SyncVar変数を変更し、変更結果を全クライアントに送る
+	[Command]
+	void CmdTellServerMyIdentity(string name)
+	{
+		playerUniqueIdentity = name;
+	}
+}
